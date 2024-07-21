f2af1bee063e9d95472b987107b7009c0d6b8f5b
gounux <contact@guilhemallaman.net>
2024-07-21 14:32:36 +0200

    add put message tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,14 +6,14 @@ from tests import TEST_RULES
 from tests.conftest import test_rooms
 
 
-def test_version(client: TestClient):
+def test_get_version(client: TestClient):
     response = client.get("/version")
     assert response.status_code == 200
     assert response.json()["version"] == get_poetry_version()
 
 
 @pytest.mark.parametrize("room", test_rooms())
-def test_status(client: TestClient, room: str):
+def test_get_status(client: TestClient, room: str):
     response = client.get("/status")
     assert response.status_code == 200
     data = response.json()
@@ -27,13 +27,39 @@ def test_status(client: TestClient, room: str):
 
 
 @pytest.mark.parametrize("room", test_rooms())
-def test_rooms(client: TestClient, room: str):
+def test_get_rooms(client: TestClient, room: str):
     response = client.get("/rooms")
     assert response.status_code == 200
     assert room in response.json()
 
 
-def test_rules(client: TestClient):
+def test_get_rules(client: TestClient):
     response = client.get("/rules")
     assert response.status_code == 200
     assert response.json()["rules"] == TEST_RULES
+
+
+@pytest.mark.parametrize("room", test_rooms())
+def test_put_message(client: TestClient, room: str):
+    response = client.put(
+        f"/room/{room}/message",
+        json={"message": "fromage", "author": f"ws-tester-{room}"},
+    )
+    assert response.status_code == 200
+    assert response.json()["message"] == "fromage"
+    assert response.json()["author"] == f"ws-tester-{room}"
+
+
+def test_put_message_wrong_room(client: TestClient):
+    assert (
+        client.put(
+            "/room/fromage/message", json={"message": "fromage", "author": "ws-tester"}
+        ).status_code
+        == 404
+    )
+    assert (
+        client.put(
+            "/room/void/message", json={"message": "fromage", "author": "ws-tester"}
+        ).status_code
+        == 404
+    )
