ed117de7eed3e78a0703e6535cd9bbab7cbe8775
gounux <contact@guilhemallaman.net>
2024-07-21 13:41:43 +0200

    setup pytest

--- gischat/models.py
+++ gischat/models.py
@@ -1,6 +1,21 @@
 from pydantic import BaseModel
 
 
+class VersionModel(BaseModel):
+    version: str
+
+
+class RoomStatusModel(BaseModel):
+    name: str
+    nb_connected_users: int
+
+
+class StatusModel(BaseModel):
+    status: str
+    healthy: bool
+    rooms: list[RoomStatusModel]
+
+
 class MessageModel(BaseModel):
     message: str
     author: str
