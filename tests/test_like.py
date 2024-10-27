e9d71d1ad8d7ab957ad53283daf2b6031ba92f9f
Guilhem Allaman <40383801+gounux@users.noreply.github.com>
2024-10-27 18:29:08 +0100

    test: split test files (#36)

---
+++ tests/test_like.py
@@ -0,0 +1,83 @@
+import pytest
+from starlette.testclient import TestClient
+
+from gischat.models import GischatMessageTypeEnum
+from tests.conftest import get_test_rooms
+
+
+@pytest.mark.parametrize("room", get_test_rooms())
+def test_websocket_like_message(client: TestClient, room: str):
+    # register client 1 (Isidore)
+    with client.websocket_connect(f"/room/{room}/ws") as websocket1:
+        assert websocket1.receive_json() == {
+            "type": GischatMessageTypeEnum.NB_USERS.value,
+            "nb_users": 1,
+        }
+        websocket1.send_json(
+            {"type": GischatMessageTypeEnum.NEWCOMER.value, "newcomer": "Isidore"}
+        )
+        assert websocket1.receive_json() == {
+            "type": GischatMessageTypeEnum.NEWCOMER.value,
+            "newcomer": "Isidore",
+        }
+
+        # register client 2 (Barnabe)
+        with client.websocket_connect(f"/room/{room}/ws") as websocket2:
+            assert websocket1.receive_json() == {
+                "type": GischatMessageTypeEnum.NB_USERS.value,
+                "nb_users": 2,
+            }
+            assert websocket2.receive_json() == {
+                "type": GischatMessageTypeEnum.NB_USERS.value,
+                "nb_users": 2,
+            }
+
+            websocket2.send_json(
+                {"type": GischatMessageTypeEnum.NEWCOMER.value, "newcomer": "Barnabe"}
+            )
+            assert websocket1.receive_json() == {
+                "type": GischatMessageTypeEnum.NEWCOMER.value,
+                "newcomer": "Barnabe",
+            }
+            assert websocket2.receive_json() == {
+                "type": GischatMessageTypeEnum.NEWCOMER.value,
+                "newcomer": "Barnabe",
+            }
+
+            # client 1 sends a message
+            websocket1.send_json(
+                {
+                    "type": GischatMessageTypeEnum.TEXT.value,
+                    "author": "Isidore",
+                    "avatar": "postgis",
+                    "text": "hi",
+                }
+            )
+            assert websocket1.receive_json() == {
+                "type": GischatMessageTypeEnum.TEXT.value,
+                "author": "Isidore",
+                "avatar": "postgis",
+                "text": "hi",
+            }
+            assert websocket2.receive_json() == {
+                "type": GischatMessageTypeEnum.TEXT.value,
+                "author": "Isidore",
+                "avatar": "postgis",
+                "text": "hi",
+            }
+
+            # client 2 likes client 1's message
+            websocket2.send_json(
+                {
+                    "type": GischatMessageTypeEnum.LIKE.value,
+                    "liker_author": "Barnabe",
+                    "liked_author": "Isidore",
+                    "message": "hi",
+                }
+            )
+            assert websocket1.receive_json() == {
+                "type": GischatMessageTypeEnum.LIKE.value,
+                "liker_author": "Barnabe",
+                "liked_author": "Isidore",
+                "message": "hi",
+            }
