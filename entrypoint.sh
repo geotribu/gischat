536bab69de6f9c78f31d247837016b1dd2bd30ac
gounux <contact@guilhemallaman.net>
2025-06-24 04:31:51 +0200

    feat(ops): make number of async worker dynamically configurable in env

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+WORKERS=${NB_UVICORN_WORKERS:-1}
+
+exec poetry run uvicorn gischat.app:app \
+    --host 0.0.0.0 \
+    --port 8000 \
+    --proxy-headers \
+    --log-config=log_config.yaml \
+    --workers $WORKERS
